3ff3f6b86752e01eaaf86ed8cfdc77a54ba2b3d2
Caleb <calebdp@gmail.com>
2019-04-28 02:57:25 -1000

    New Age

--- Hello/hello.java
+++ Hello/hello.java
@@ -4,7 +4,9 @@ public class hello
 	public static void main(String[]args)
 	{
 		String msg = "";
+		int age = 18;
 		System.out.println(greeting(msg));
+		System.out.println("I am "+age+" years old.");
 	}
 	
 	public static String greeting(String msg)
