c93814af426ca7288fce23a31014ccbb80fd9f08
Caleb <calebdp@gmail.com>
2019-04-28 03:14:50 -1000

    SchoolTwo

--- Hello/hello.java
+++ Hello/hello.java
@@ -4,7 +4,8 @@ public class hello
 	public static void main(String[]args)
 	{
 		String msg = "";
-		System.out.println(greeting(msg));
+		System.out.println(greeting(msg))
+		System.out.print("Tell me about yourself.");;
 	}
 	
 	public static String greeting(String msg)
